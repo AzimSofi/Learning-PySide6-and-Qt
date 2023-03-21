ad5eba5930c8547c49bd0e04f7a8370045e67b89
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-03-21 20:45:52 +0800

    Update button_holder.py

--- button_holder.py
+++ button_holder.py
@@ -4,9 +4,20 @@ from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 class ButtonHolder(QMainWindow): # Inherits from QMainWindow (parent) class
     def __init__(self): # Constructor for ButtonHolder (daughter class)
         super().__init__() # Inheriting QMainWindows' constructor
-
-        self.setWindowTitle("テスト")
+        self.setWindowTitle("window_title")
+        
+        #Button
         button = QPushButton("Press")
+        button.setCheckable(True) # Button is a toggle
+        self.setCentralWidget(button) # Set button as central widget
+        
+    def set_window_title(self, window_title):
+        self.setWindowTitle(window_title)
 
-        # Set button as central widget
-        self.setCentralWidget(button)
+"""
+def respond_to_slider(data):
+    print ("slider moved to : ", data)
+
+    slider = QSlider(Qt.Horizontal)
+    slider.setMinimum(1)
+"""
